0ca836286843562596c189d333814476a9282f71
wangsong26 <wangsong26@163.com>
2020-10-22 18:55:51 +0800

    commit py

--- test6.py
+++ test6.py
@@ -1,16 +1,37 @@
-import os
-import fnmatch
-from openpyxl import load_workbook
-import openpyxl
-def return_object(path):
-    all_file = fnmatch.filter(os.listdir(path), '*.txt')
-    print(all_file)
-    for file in all_file:
-        print(file)
-        lst = []
-        with open(f'./report/{file}', 'r', encoding='utf-8') as f:
-            for line in f.readlines():
-                line = line.strip()
-                lst.append(line)
-        print(f'name{file}--- {lst}')
-return_object('./report')
+import requests
+import json
+
+"""
+Modify these please
+"""
+#For NXAPI to authenticate the client using client certificate, set 'client_cert_auth' to True.
+#For basic authentication using username & pwd, set 'client_cert_auth' to False.
+client_cert_auth=False
+switchuser='admin'
+switchpassword='cisco!123'
+client_cert='PATH_TO_CLIENT_CERT_FILE'
+client_private_key='PATH_TO_CLIENT_PRIVATE_KEY_FILE'
+ca_cert='PATH_TO_CA_CERT_THAT_SIGNED_NXAPI_SERVER_CERT'
+
+url='http://10.122.188.73:10080/ins'
+myheaders={'content-type':'application/json'}
+payload={
+  "ins_api": {
+    "version": "1.0",
+    "type": "cli_show",
+    "chunk": "0",
+    "sid": "sid",
+    "input": "show module",
+    "output_format": "json"
+  }
+}
+
+if client_cert_auth is False:
+    response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
+else:
+    url='https:///ins'
+    response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword),cert=(client_cert,client_private_key),verify=ca_cert).json()
+
+len1=len(response['ins_api']['outputs']['output']['body']['TABLE_modmacinfo']['ROW_modmacinfo'])
+for i in range(len1):
+    print(response['ins_api']['outputs']['output']['body']['TABLE_modmacinfo']['ROW_modmacinfo'][i]['serialnum'])
